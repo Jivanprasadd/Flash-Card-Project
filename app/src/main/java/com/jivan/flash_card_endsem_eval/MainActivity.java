3d81dc45c8769e48a3b310b28644c7d76fd97325
Jivan Prasadd <jivanprasadd2004@gmail.com>
2024-11-09 09:42:45 +0530

    Initial Comment

---
+++ app/src/main/java/com/jivan/flash_card_endsem_eval/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jivan.flash_card_endsem_eval;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/com/jivan/flash_card_endsem_eval/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.jivan.flash_card_endsem_eval;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
